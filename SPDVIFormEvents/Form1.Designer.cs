6a4f3ec4c80ae4af3dfdcc4595bcf377d5add647
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-11-05 19:58:04 +0100

    Finished project SPDVIFormEvents. In this project we have a form with diferent objects in there and we test some of Events and EventHandlers provided by C#.

--- SPDVIFormEvents/Form1.Designer.cs
+++ SPDVIFormEvents/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SPDVIFormEvents
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messagesTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // messagesTextBox
+            // 
+            this.messagesTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.messagesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messagesTextBox.ForeColor = System.Drawing.Color.White;
+            this.messagesTextBox.Location = new System.Drawing.Point(703, 12);
+            this.messagesTextBox.Multiline = true;
+            this.messagesTextBox.Name = "messagesTextBox";
+            this.messagesTextBox.ReadOnly = true;
+            this.messagesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messagesTextBox.Size = new System.Drawing.Size(809, 963);
+            this.messagesTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Double click me!";
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(31, 118);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(184, 40);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 33;
+            this.listBox1.Items.AddRange(new object[] {
+            "First line",
+            "Second line",
+            "Third line"});
+            this.listBox1.Location = new System.Drawing.Point(31, 225);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 103);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(31, 407);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 40);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.GotFocus += new System.EventHandler(this.textBox1_GotFocus);
+            this.textBox1.LostFocus += new System.EventHandler(this.textBox1_LostFocus);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "https://www.elagoradiario.com/wp-content/uploads/2020/06/Retrato-corto-de-una-peq" +
+    "ue%C3%B1a-nutria-asi%C3%A1tica-u-oriental-Aonyx-cinerea-Gary-Perkin-1024x683.jpg" +
+    "";
+            this.pictureBox1.Location = new System.Drawing.Point(31, 540);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1526, 986);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messagesTextBox);
+            this.Name = "mainForm";
+            this.Text = "Windows Forms Components - Common Events";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainForm_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox messagesTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- SPDVIFormEvents/Form1.cs
+++ SPDVIFormEvents/Form1.cs
@@ -10,11 +10,75 @@ using System.Windows.Forms;
 
 namespace SPDVIFormEvents
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(780, 550);
+            addLine("Main Form loaded.");
+        }
+
+        private void mainForm_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) { addLine("Left button of the mouse has been clicked over the main form."); }
+            if (e.Button == MouseButtons.Left) { addLine("Right button of the mouse has been clicked over the main form."); ; }
+        }
+
+        void addLine(string line)
+        {
+            messagesTextBox.AppendText(line+"\r\n");
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            addLine("System has detected a Double Click on the label.");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            addLine("Numeric Up Down value changed to "+numericUpDown1.Value);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            addLine("List Box Selected Index has changed to " + listBox1.SelectedIndex);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_GotFocus(object sender, EventArgs e)
+        {
+            addLine("textBox1 has got the focus now.");
+        }
+        private void textBox1_LostFocus(object sender, EventArgs e)
+        {
+            addLine("textBox1 has lost the focus.");
+        }
+        public char pressed;
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            pressed = e.KeyChar;
+        }
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(!e.KeyData.ToString().Equals("Shift") && !e.KeyData.ToString().Equals("ShiftKey"))
+                addLine("The key '" + pressed + "' has been pressed and released.");
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            addLine("The mouse pointer is inside the image.");
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            addLine("The mouse pointer is outside the image.");
         }
     }
 }
